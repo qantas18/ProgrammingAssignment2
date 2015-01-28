628571340c263e0fde901f5f8790e9fc16827276
Marvin <qantas18@gmail.com>
2015-01-28 17:56:38 +0800

    First time commit!

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,38 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y){
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
 ## Write a short comment describing this function
+## Below is a short sample for testing this function
+## a <- matrix(sample(16),4,4)
+## m1 <- makeCacheMatrix(a)
+## cacheSolve(m1)
+##
+## inv <- cacheSolve(m1) ## This time, you should get the message: getting cached data
+## a %*% inv ## verify whether equal to an identity matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if(!is.null(m)){
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
 }
